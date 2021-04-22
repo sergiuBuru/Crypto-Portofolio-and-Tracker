9a7fa059e77e3e119c3ee778e46f56d36e96d754
Sergiu Buruiana <sergiu.buru99@gmail.com>
2021-04-22 16:34:04 -0500

    Added drawer tabs with icons. Added event listeners for each drawer tab. Added nav bar icon for each screen

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const nav = mdc.ripple.MDCRipple.attachTo(topAppBarElement);
+const nav = new mdc.topAppBar.MDCTopAppBar(topAppBarElement);
 
 const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
 const listEl = document.querySelector('.mdc-drawer .mdc-list');
 const mainContentEl = document.querySelector('main');
-
+const drawerScrim = document.querySelector(".mdc-drawer-scrim");
 const burgerButton = document.querySelector("#nav-burgerbutton");
+
 burgerButton.addEventListener("click", (event) => {
     drawer.open = true;
+    burgerButton.classList.remove("burger-no-ripple");
 });
 
 listEl.addEventListener('click', (event) => {
@@ -16,4 +18,35 @@ listEl.addEventListener('click', (event) => {
 
 document.body.addEventListener('MDCDrawer:closed', () => {
   //mainContentEl.querySelector('input, button').focus();
+});
+
+const homeButton = document.querySelector("#home-button");
+const newsButton = document.querySelector("#news-button");
+const trendingButton = document.querySelector("#trending-button");
+const investmentsButton = document.querySelector("#investments-button");
+const container = document.querySelector(".container");
+const barIcon = document.querySelector("#bar-icon");
+
+homeButton.addEventListener("click", (event) => {
+  container.innerHTML = "<h1>Home</h1>"
+  barIcon.innerHTML = "home";
+  burgerButton.classList.add("burger-no-ripple");
+});
+
+newsButton.addEventListener("click", (event) => {
+  container.innerHTML = "<h1>News</h1>"
+  barIcon.innerHTML = "announcements";
+  burgerButton.classList.add("burger-no-ripple");
+});
+
+trendingButton.addEventListener("click", (event) => {
+  container.innerHTML = "<h1>trending</h1>";
+  barIcon.innerHTML = "whatshot";
+  burgerButton.classList.add("burger-no-ripple");
+});
+
+investmentsButton.addEventListener("click", (event) => {
+  container.innerHTML = "<h1>investments</h1>";
+  barIcon.innerHTML = "timeline";
+  burgerButton.classList.add("burger-no-ripple");
 });
